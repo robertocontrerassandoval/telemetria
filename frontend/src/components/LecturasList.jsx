5cc66faf5771f557ee17ba1e5f73034e3058ec30
Roberto Contreras <contreras_sandoval@hotmail.com>
2025-08-26 12:19:09 -0400

    update

--- frontend/src/components/LecturasList.jsx
+++ frontend/src/components/LecturasList.jsx
@@ -1,14 +1,9 @@
 // src/components/LecturasList.jsx
 import React from 'react';
-
 export default function LecturasList({ lecturas }) {
-  const ultimas10 = lecturas
-    .slice(-10) // toma las últimas 10 (del final)
-    .reverse(); // las pone de la más reciente a la más antigua
-
   return (
     <ul>
-      {ultimas10.map((l, i) => (
+      {lecturas.map((l, i) => (
         <li key={i}>
           {new Date(l.fecha).toLocaleString()} — Temp: {l.temperatura}°C — Humedad: {l.humedad}%
         </li>
